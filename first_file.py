ce52a5f66d05f455e74e8a0160d7dfdca7343e45
Rej Flores <flores.rejeenald@gmail.com>
2020-12-07 14:53:38 +0800

    Ticket101 added more paramters in sum method

--- first_file.py
+++ first_file.py
@@ -2,9 +2,9 @@ print("Hello, world!")
 print("Hello, Earth!")
 print("Hello, Uranus!")
 
-def sum(x, y):
-    return x + y
+def sum(x, y, z, y):
+    return x + y + z + y
 
 if __name__ == "__main__":
-    s = sum(1, 2)
+    s = sum(1, 2, 3, 4)
     print("The sum is: %s" % s)
