6b531fc65a31d545c4b170f695db2e7734bd539c
Rej Flores <flores.rejeenald@gmail.com>
2020-12-07 15:16:45 +0800

    Ticket101 revert changes

--- first_file.py
+++ first_file.py
@@ -1,3 +1,10 @@
 print("Hello, world!")
 print("Hello, Earth!")
 print("Hello, Uranus!")
+
+def div(x, y):
+    return x/y
+
+if __name__ == "__main__":
+    s = div(1,2)
+    print("The sum is: %s" % s)
