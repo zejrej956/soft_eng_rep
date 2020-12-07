4163ed95418833b405aaad7bbe5289d6185b62ca
Rej Flores <flores.rejeenald@gmail.com>
2020-12-07 15:05:20 +0800

    Ticket101 change to div

--- first_file.py
+++ first_file.py
@@ -2,9 +2,9 @@ print("Hello, world!")
 print("Hello, Earth!")
 print("Hello, Uranus!")
 
-def sum(x, y, z, y):
-    return x + y + z + y
+def div(x, y):
+    return x/y
 
 if __name__ == "__main__":
-    s = sum(1, 2, 3, 4)
+    s = div(1,2)
     print("The sum is: %s" % s)
