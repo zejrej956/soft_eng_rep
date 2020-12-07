4fa103bf483c97268cc8b1dcaa8bde42b40949dd
Rej Flores <flores.rejeenald@gmail.com>
2020-12-07 15:04:43 +0800

    Ticket101 change to product

--- first_file.py
+++ first_file.py
@@ -2,9 +2,9 @@ print("Hello, world!")
 print("Hello, Earth!")
 print("Hello, Uranus!")
 
-def sum(x, y, z, y):
-    return x + y + z + y
+def sum(x, y, z):
+    return x + y + z
 
 if __name__ == "__main__":
-    s = sum(1, 2, 3, 4)
+    s = product(1, 2, 3)
     print("The sum is: %s" % s)
